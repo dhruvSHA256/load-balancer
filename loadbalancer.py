3a7841e93e253d9c245be3b3027fb9dc95b768d7
DHruv <dhruv.sha256@gmail.com>
2023-11-01 00:28:41 +0530

    refactor

--- loadbalancer.py
+++ loadbalancer.py
@@ -19,29 +19,40 @@ PORT = 5432
 # for a particular client request we need to make sure we are sending the all packets to the same server
 
 
+class BackendServer:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.alive = True
+
+    def connect(self):
+        self.backend_conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.backend_conn.connect((self.host, self.port))
+
+    # client_conn -> lb <- backend_conn
+    def reverseProxy(self, client_conn):
+        def forward_request(source, destination):
+            print(f"Sending data from {source.getsockname()} to {destination.getsockname()}")
+            try:
+                while True:
+                    data = source.recv(1024)
+                    if len(data) == 0:
+                        break
+                    destination.send(data)
+            finally:
+                source.close()
+                destination.close()
+
+        c2b_thread = threading.Thread(target=forward_request, args=(client_conn, self.backend_conn))
+        b2c_thread = threading.Thread(target=forward_request, args=(self.backend_conn, client_conn))
+        c2b_thread.start()
+        b2c_thread.start()
+        c2b_thread.join()
+        b2c_thread.join()
+
+
 def select_server():
     return "127.0.0.1", 5001
-
-
-def reverse_proxy(client_conn, backend_conn):
-    def forward_request(source, destination):
-        print(f"Sending data from {source.getsockname()} to {destination.getsockname()}")
-        try:
-            while True:
-                data = source.recv(1024)
-                if len(data) == 0:
-                    break
-                destination.send(data)
-        finally:
-            source.close()
-            destination.close()
-
-    c2b_thread = threading.Thread(target=forward_request, args=(client_conn, backend_conn))
-    b2c_thread = threading.Thread(target=forward_request, args=(backend_conn, client_conn))
-    c2b_thread.start()
-    b2c_thread.start()
-    c2b_thread.join()
-    b2c_thread.join()
 
 
 def main():
@@ -56,13 +67,11 @@ def main():
                 print(f"Connected by {client_addr}")
                 # whenever we accept new client connection
                 # we will load balance it
-                backend_host, backend_port = select_server()
-                backend_conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                backend_conn.connect((backend_host, backend_port))
-                # so now client_conn -> lb <- backend_conn
-                # we need to redirect requests
-                # first make sort of a reverse proxy
-                reverse_proxy(client_conn, backend_conn)
+                # select_server will make sure server is healthy
+                server_host, server_port = select_server()
+                backend = BackendServer(server_host, server_port)
+                backend.connect()
+                backend.reverseProxy(client_conn)
 
     finally:
         sock.close()
